9d74be92fdd92af49de3be5fd4f74a392ec6f898
Rae Ho <raeyean@email.com>
2023-12-08 21:36:06 +1100

    wip updated post handler

--- src/app/api/v1/quizzes/route.ts
+++ src/app/api/v1/quizzes/route.ts
@@ -13,8 +13,29 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request, response: Response ) {
   // add new quiz to database
-  console.log(request)
+  console.log(request, response)
   
   // const data = await db.insert(quizzes).values({name: `${request.body}`}).returning().onConflictDoNothing()
-  return new NextResponse(JSON.stringify(response))
+  // return new NextResponse(JSON.stringify(response))
+
+  try {
+    // Log the request information to the server console
+    console.log("Received POST request:", request);
+
+    // Parse the request body as JSON
+    const requestData = await request.json();
+
+    // Add a new quiz to the database
+    const newQuiz = await db.insert(quizzes).values({ name: requestData.name }).returning();
+
+    // Log the newly created quiz
+    console.log("Created quiz:", newQuiz);
+
+    return new NextResponse(JSON.stringify(newQuiz), {
+      status: 201, // 201 Created status code for successful creation
+    });
+  } catch (error) {
+    console.error("Error creating quiz:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
